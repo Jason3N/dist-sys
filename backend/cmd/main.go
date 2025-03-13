5b45bd46e18858bf0dc92dc221ca84005e128da0
jason3N <jasonnguyenphhs@gmail.com>
2025-03-12 17:19:45 -0700

    structure needed to test out using multiple cores + concurrency

---
+++ backend/cmd/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+
+	"github.com/ericlagergren/decimal"
+)
+
+// enable parralelism by using GOMAXPROCS
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	// router
+	// r := mux.NewRouter()
+	start := time.Now()
+	// enable concurrency
+	fmt.Printf("Total time: %v\n", time.Since(start))
+	// have low-level, medium-level, high-level
+	// r.HandleFunc("/highLevel", heavyHandler)
+	// r.HandleFunc("/mediumLevel", mediumHandler)
+	// r.HandleFunc("/lowLevel", lowHandler)
+
+	// err := http.ListenAndServe(":8080", r)
+	// if err != nil {
+	// 	fmt.Printf("error running server")
+	// 	return
+	// }
+}
+
+// mutex
+func handleConcurrency() {
+	var wg sync.WaitGroup
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go heavyHandler(&wg)
+	}
+	wg.Wait()
+}
+
+func heavyHandler(wg *sync.WaitGroup) {
+	defer wg.Done()
+	// 1 second
+	time.Sleep(1 * time.Second)
+}
+
+func mediumHandler(wg *sync.WaitGroup) {
+	defer wg.Done()
+	// 0.5 second
+	mediumTime := new(decimal.Big).SetFloat64(0.5)
+	mediumTimeFloat, _ := mediumTime.Float64()
+	sleepDuration := time.Duration(mediumTimeFloat * float64(time.Second))
+	time.Sleep(sleepDuration)
+}
+
+func lowHandler(wg *sync.WaitGroup) {
+	defer wg.Done()
+	// 0.25 second
+	lowTime := new(decimal.Big).SetFloat64(0.5)
+	lowFloat, _ := lowTime.Float64()
+	sleepDuration := time.Duration(lowFloat * float64(time.Second))
+	time.Sleep(sleepDuration)
+}
---
+++ backend/go.mod
@@ -0,0 +1,8 @@
+module github.com/Jason3N/super-duper-high-dist-sys.git
+
+go 1.23.4
+
+require (
+	github.com/ericlagergren/decimal v0.0.0-20240411145413-00de7ca16731 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+)
